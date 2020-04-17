5a17344390601dd182cbeb4508ce8794575a89fd
Matheus Mendes <matheusshmendes@gmail.com>
2020-04-17 19:25:28 -0300

    Add wireframe

---
+++ src/components/VideosListItem/index.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const VideoItem = styled.div`
+  display: flex;
+  background-color: white;
+  border: 1px solid black;
+  padding: 5px;
+`;
+
+export const VideoTitle = styled.h4`
+  margin-top: 0px;
+`;
+
+export const VideoDescription = styled.p``;
+
+export const VideoThumbnail = styled.img`
+  width: 120px;
+  height: 90px;
+`;
+
+export const VideoInformations = styled.div`
+  padding: 10px;
+`;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
-import App from './pages/App';
+import App from './pages/App/index';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
