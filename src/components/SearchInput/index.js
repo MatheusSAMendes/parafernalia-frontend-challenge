f7795c29b096d0d1d6827b4fafd114fac1ff21d3
Matheus Mendes <matheusshmendes@gmail.com>
2020-04-19 17:34:42 -0300

    Add component style

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { SearchField } from './index.styled';
+import { SearchField, SearchButton } from './index.styled';
 
 const SearchInput = ({ query, onSearchSubmit, onQueryChange }) => {
   const handleSearchSubmit = (event) => {
@@ -11,7 +11,7 @@ const SearchInput = ({ query, onSearchSubmit, onQueryChange }) => {
   return (
     <form onSubmit={handleSearchSubmit}>
       <SearchField type="search" id="search" name="search" placeholder="Search videos on YouTube" value={query} onChange={onQueryChange} />
-      <button type="submit" class="btn btn-primary">Search</button>
+      <SearchButton type="submit" class="btn btn-primary">Search</SearchButton>
     </form>
   );
 }
