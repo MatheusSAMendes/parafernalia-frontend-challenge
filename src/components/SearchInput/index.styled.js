5245de4329526d4f01a6fea5161d6b358cdc0a90
Matheus Mendes <matheusshmendes@gmail.com>
2020-04-19 17:35:59 -0300

    Add component style (#14)

--- src/components/SearchInput/index.styled.js
+++ src/components/SearchInput/index.styled.js
@@ -3,9 +3,22 @@ import styled from 'styled-components';
 export const SearchField = styled.input`
   padding: 0.5em;
   margin-left: 60px;
-  margin-right: 5px;
   border: 2px solid #f9f9f9;
   background-color: #f9f9f9;
-  border-radius: 3px;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2px 4px 0 rgba(0, 0, 0, 0.19);
   width: 350px;
+  `;
+  
+  export const SearchButton = styled.button`
+  width: 50;
+  padding: 0.5em;
+  border: 2px solid #7f0000;
+  background-color: #7f0000;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2px 4px 0 rgba(0, 0, 0, 0.19);
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+  color: #f9f9f9;
+
 `;
