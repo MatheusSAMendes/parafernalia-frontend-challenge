ef9d8a3f1322302779c2d1d90ec4fb8e70e1f28a
Matheus Mendes <matheusshmendes@gmail.com>
2020-04-20 11:22:11 -0300

    Add react router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from "history";
 
-import App from './pages/App/index';
+import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const history = createBrowserHistory();
+
+ReactDOM.render((
+  <Router history={history}>
+    <Route exactpath='/' component={App}>
+    </Route>
+  </Router>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
